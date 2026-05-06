2838f214164aff473f74976a1259a6528d20000f
ashwinka <32065323+ashwinka@users.noreply.github.com>
2026-05-06 16:25:19 +0530

    Create AppConstants.java

---
+++ backend/src/main/java/com/metadyna/AppConstants.java
@@ -0,0 +1,17 @@
+package com.metadyna;
+
+public class AppConstants {
+    public static final String DATA_TYPE_TXT = "TEXT";
+    public static final String DATA_TYPE_NUM = "NUMBER";
+    public static final String DATA_TYPE_DATE = "DATE";
+    public static final String DATA_TYPE_TIME = "DATETIME";
+    public static final String DATA_TYPE_FILE = "FILE";
+
+
+    public static final String SAVE_ISSUE_SEV_ERR = "ERROR";
+    public static final String SAVE_ISSUE_SEV_WAR = "WARNING";
+    public static final String SAVE_ISSUE_SEV_INFO = "INFO";
+
+    public static final String SAVE_ISSUE_DB_RULE = "DB_CONSTRAINTS";
+
+}
